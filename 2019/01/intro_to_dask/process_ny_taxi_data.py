6952f5f9f87034ec4bd15bde663aa71b8a8cbb93
eric.ness <eric.ness@wheniwork.com>
2019-01-08 17:57:57 -0600

    Added data processing steps

--- 2019/01/intro_to_dask/process_ny_taxi_data.py
+++ 2019/01/intro_to_dask/process_ny_taxi_data.py
@@ -5,8 +5,34 @@ cluster = LocalCluster(diagnostics_port=0, n_workers=1, processes=False)
 client = Client(cluster)
 
 df = dd.read_csv(
-    's3://nyc-tlc/trip data/yellow_tripdata_2018-01.csv',
-    storage_options={'anon': True, 'use_ssl': False}
+#    's3://nyc-tlc/trip data/yellow_tripdata*.csv',
+#    storage_options={'anon': True, 'use_ssl': False}
+     '/Users/ericness/github/ericness/blog/2019/01/intro_to_dask/data/yellow_tripdata*.csv'
 )
 
-print(df.head())
+print(df)
+
+cost_distribution = (
+     df[['tpep_pickup_datetime', 'tpep_dropoff_datetime', 'trip_distance', 'total_amount']]
+     .astype({'tpep_pickup_datetime': 'datetime64[ms]', 'tpep_dropoff_datetime': 'datetime64[ms]'})
+     .assign(drive_time=lambda df: (df.tpep_dropoff_datetime - df.tpep_pickup_datetime).dt.seconds // 300)
+     .drop(['tpep_pickup_datetime', 'tpep_dropoff_datetime'], axis=1)
+     .round({'trip_distance': 0})
+     .groupby(['drive_time', 'trip_distance'])
+     .mean()
+     .rename(columns={'total_amount': 'avg_amount'})
+)
+
+
+
+print(
+     cost_distribution
+     .compute()
+     .reset_index()
+     .pivot(
+          index='trip_distance',
+          columns='drive_time',
+          values='avg_amount',
+          fill_value=0
+     )
+)
