8cc0fdaee453ab803e6b57e2e0bbb97e9998eff1
eric.ness <eric.ness@wheniwork.com>
2019-02-15 18:05:23 -0600

    Finished formatting code

--- 2019/01/intro_to_dask/process_ny_taxi_data.py
+++ 2019/01/intro_to_dask/process_ny_taxi_data.py
@@ -1,18 +1,24 @@
 import numpy as np
 import pandas as pd
-from dask.distributed import Client, LocalCluster
+from dask.distributed import Client
 import dask.dataframe as dd
 import math
 
 
 def roundup(x, base: int = 5):
+    """Round `x` up to nearest `base`"""
     return int(math.ceil(x / float(base))) * base
 
 
-def transform_dask_dataframe(taxi_data: dd) -> dd:
+def round_series_up(s: dd.Series) -> dd.Series:
+    """Apply roundup function to all elements of `s`"""
+    return s.apply(roundup, meta=pd.Series(data=[], dtype=np.float32))
+
+
+def transform_dask_dataframe(df: dd.DataFrame) -> dd.DataFrame:
     """Process NYC taxi data"""
     return (
-        taxi_data[[
+        df[[
             'tpep_pickup_datetime', 'tpep_dropoff_datetime',
             'trip_distance', 'total_amount'
         ]]
@@ -20,11 +26,11 @@ def transform_dask_dataframe(taxi_data: dd) -> dd:
             'tpep_pickup_datetime': 'datetime64[ms]',
             'tpep_dropoff_datetime': 'datetime64[ms]'
         })
-        .assign(drive_time=(lambda df:
-            (df.tpep_dropoff_datetime - df.tpep_pickup_datetime).dt.seconds
+        .assign(drive_time=(lambda df: (
+            df.tpep_dropoff_datetime - df.tpep_pickup_datetime).dt.seconds
             // 300))
-        .assign(drive_time=lambda df: df.drive_time.apply(roundup, meta=pd.Series(data=[], dtype=np.float32)))
-        .assign(trip_distance=lambda df: df.trip_distance.apply(roundup, meta=pd.Series(data=[], dtype=np.float32)))
+        .assign(drive_time=lambda df: round_series_up(df.drive_time))
+        .assign(trip_distance=lambda df: round_series_up(df.trip_distance))
         .query('drive_time <= 120 & trip_distance <= 50')
         .drop(['tpep_pickup_datetime', 'tpep_dropoff_datetime'], axis=1)
         .round({'trip_distance': 0})
@@ -34,11 +40,10 @@ def transform_dask_dataframe(taxi_data: dd) -> dd:
     )
 
 
-def compute_final_dataframe(taxi_data: dd) -> pd.DataFrame:
+def compute_final_dataframe(df: dd.DataFrame) -> pd.DataFrame:
     """Execute dask task graph and compute final results"""
-
     return (
-        taxi_data
+        df
         .compute()
         .reset_index()
         .pivot(
@@ -53,12 +58,12 @@ def compute_final_dataframe(taxi_data: dd) -> pd.DataFrame:
 if __name__ == "__main__":
     client = Client()
 
-    df = dd.read_csv(
-            's3://nyc-tlc/trip data/yellow_tripdata_2018-04.csv',
-            storage_options={'anon': True, 'use_ssl': False}
+    taxi_data = dd.read_csv(
+        's3://nyc-tlc/trip data/yellow_tripdata_2018-04.csv',
+        storage_options={'anon': True, 'use_ssl': False}
     )
 
-    transformed_data = transform_dask_dataframe(df)
-    cost_distribution = compute_final_dataframe(transformed_data)
+    transformed_data = transform_dask_dataframe(taxi_data)
+    fare_distribution = compute_final_dataframe(transformed_data)
 
-    print(cost_distribution.to_string())
+    print(fare_distribution.to_string())
