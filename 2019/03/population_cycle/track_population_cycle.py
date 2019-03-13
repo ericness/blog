d295996e4ecc5e8b906173be235ce97381ef4318
eric.ness <eric.ness@wheniwork.com>
2019-03-13 17:19:16 -0500

    Creating Fake Data
    Got matplotlib working. Committing before changing over to plotly.

--- 2019/03/population_cycle/track_population_cycle.py
+++ 2019/03/population_cycle/track_population_cycle.py
@@ -3,14 +3,11 @@ from sklearn.datasets import make_regression
 from matplotlib import pyplot
 
 # generate regression dataset
-X = np.linspace(0, 3)
-y = np.sin(X)
-# plot regression dataset
-pyplot.scatter(X, y)
+X = np.linspace(0, 3.1415, num=20)
 
 for i in range(5):
     skew = np.random.rand() / 2 + 0.5
     skew = skew if np.random.rand() > 0.5 else -skew
-    y2 = np.sin(X - skew * np.sin(X)) * np.random.rand()
-    pyplot.scatter(X, y2)
+    y = (np.sin(X - skew * np.sin(X)) * 2 * np.random.rand() + np.random.normal(scale=0.1, size=20)).clip(min=0)
+    pyplot.scatter(X, y)
 pyplot.show()
