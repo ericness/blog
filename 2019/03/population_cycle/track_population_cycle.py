ada84529bc6e0a8462c372a0b5bee29099111894
eric.ness <eric.ness@wheniwork.com>
2019-03-14 17:34:40 -0500

    Creating Fake Data
    Spliced fake data sets on to datetime indexes.

--- 2019/03/population_cycle/track_population_cycle.py
+++ 2019/03/population_cycle/track_population_cycle.py
@@ -1,13 +1,38 @@
+from datetime import datetime
+from datetime import timedelta
 import numpy as np
-from sklearn.datasets import make_regression
-from matplotlib import pyplot
+import pandas as pd
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-# generate regression dataset
-X = np.linspace(0, 3.1415, num=20)
+data = []
 
-for i in range(5):
-    skew = np.random.rand() / 2 + 0.5
+for i in range(10):
+    start_date = datetime(2018, 1, 1) + timedelta(days=int(np.random.rand() * 90))
+    end_date = start_date + timedelta(days=int(np.random.rand() * 90))
+
+    n_days = (end_date - start_date).days + 1
+    X = np.linspace(0, 3.1415, num=n_days)
+
+    skew = np.random.rand() * 1 # / 2 + 0.5
     skew = skew if np.random.rand() > 0.5 else -skew
-    y = (np.sin(X - skew * np.sin(X)) * 2 * np.random.rand() + np.random.normal(scale=0.1, size=20)).clip(min=0)
-    pyplot.scatter(X, y)
-pyplot.show()
+    y = (np.sin(X - skew * np.sin(X)) * 1.5 * (np.random.rand() + 0.5) + np.random.normal(scale=0.1, size=n_days)
+         ).clip(min=0)
+
+    df = pd.DataFrame(
+        data={'value': y},
+        index=pd.MultiIndex.from_product(
+            [pd.date_range(start=start_date, end=end_date), [i]],
+            names=['date', 'customer']
+        )
+    )
+
+    print(df)
+
+    trace = go.Scatter(x=df.index.get_level_values('date'), y=df['value'])
+    data.append(trace)
+
+
+plotly.offline.init_notebook_mode(connected=True)
+plotly.offline.plot(data)
