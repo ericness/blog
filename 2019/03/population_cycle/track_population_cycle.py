3d24a1a0e081a05ee3780dfdd06adddf9b188c39
eric.ness <eric.ness@wheniwork.com>
2019-03-16 18:11:33 -0500

    Creating Fake Data
    Used interpolation to smooth out the noise that was applied. Got it to a place that I'm happy with.

--- 2019/03/population_cycle/track_population_cycle.py
+++ 2019/03/population_cycle/track_population_cycle.py
@@ -10,15 +10,18 @@ data = []
 
 for i in range(10):
     start_date = datetime(2018, 1, 1) + timedelta(days=int(np.random.rand() * 90))
-    end_date = start_date + timedelta(days=int(np.random.rand() * 90))
+    end_date = start_date + timedelta(days=int(np.random.rand() * 90) + 60)
 
     n_days = (end_date - start_date).days + 1
     X = np.linspace(0, 3.1415, num=n_days)
+    X_coarse = np.linspace(0, 3.1415, num=12)
 
-    skew = np.random.rand() * 1 # / 2 + 0.5
+    skew = np.random.rand() / 2
     skew = skew if np.random.rand() > 0.5 else -skew
-    y = (np.sin(X - skew * np.sin(X)) * 1.5 * (np.random.rand() + 0.5) + np.random.normal(scale=0.1, size=n_days)
-         ).clip(min=0)
+    y_skew = np.sin(X - skew * np.sin(X))
+    y_magnitude = 1.5 * (np.random.rand() + 0.5) * y_skew
+    y_noise = np.interp(X, X_coarse, np.random.normal(scale=0.3, size=12)) + y_magnitude
+    y = y_noise.clip(min=0)
 
     df = pd.DataFrame(
         data={'value': y},
@@ -27,8 +30,6 @@ for i in range(10):
             names=['date', 'customer']
         )
     )
-
-    print(df)
 
     trace = go.Scatter(x=df.index.get_level_values('date'), y=df['value'])
     data.append(trace)
