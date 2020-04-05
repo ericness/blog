26dd17c22b6da065cd9e5ba12b985bf4927403b4
nesseri <eric.ness@chrobinson.com>
2020-04-05 16:36:07 -0500

    Testing flask_restx App

---
+++ 2020/03/flask_rest_app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restx import Api, Resource
+
+
+def create_app():
+    """Initialize the Flask app"""
+    app = Flask(__name__)
+    api = Api()
+
+    with app.app_context():
+        # Initialize globals
+        api.init_app(app)
+
+    @api.route('/hello')
+    class HelloWorld(Resource):
+        def get(self):
+            return {'hello': 'world'}
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
---
+++ 2020/03/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+
+import flask_rest_app
+
+
+@pytest.fixture
+def client():
+    app = flask_rest_app.create_app()
+    yield app.test_client()
+
+
+def test_get_hello(client):
+    """Should return expected output from /hello"""
+    assert client.get('/hello').json == {"hello": "world"}
